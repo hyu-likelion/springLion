87fda24db175a52d4fee99c8b034931421db266d
Sanghyo <sanghyoboy@naver.com>
2021-06-01 12:36:48 +0900

    response controller

---
+++ sanghyo/week3/springmvc/src/main/java/hello/springmvc/basic/response/ResponseViewController.java
@@ -0,0 +1,27 @@
+package hello.springmvc.basic.response;
+
+import org.springframework.boot.Banner;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class ResponseViewController {
+
+    @RequestMapping("/response-view-v1")
+    public ModelAndView responseV1(){
+        ModelAndView mv = new ModelAndView("response/hello")
+                .addObject("data", "hello!");
+
+        return mv;
+    }
+
+    @RequestMapping("/response-view-v2")
+    public String responseV2(Model model){
+        model.addAttribute("data", "hello!");
+
+        return "response/hello";
+    }
+
+}
