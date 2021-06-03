7556ab87e86544efd7b22c1ec581f89c1847af3a
Sanghyo <sanghyoboy@naver.com>
2021-06-03 16:44:17 +0900

    thymeleaf practice

---
+++ sanghyo/week4/item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -0,0 +1,44 @@
+package hello.itemservice.web.basic;
+
+import hello.itemservice.domain.item.Item;
+import hello.itemservice.domain.item.ItemRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.annotation.PostConstruct;
+import java.util.List;
+
+@Controller
+@RequestMapping("c")
+@RequiredArgsConstructor
+public class BasicItemController {
+
+    private final ItemRepository itemRepository;
+
+//    @Autowired
+//    public BasicItemController(ItemRepository itemRepository) {
+//        this.itemRepository = itemRepository;
+//    }
+//    RequiredArgsConstructor annotation때문에 final이 있는 멤버변수만 생성자 자동생성 하기 때문에 생략 가능
+    // 생성자가 딱 1개만 있는 경우 해당 생성자에 @AutoWired로 의존관계 주입
+
+    @GetMapping
+    public String items(Model model){
+        List<Item> items = itemRepository.findAll();
+        model.addAttribute("items", items);
+        return "basic/items";
+    }
+
+    /**
+     * 테스트용 데이터 추가
+     */
+    @PostConstruct
+    public void init() {
+        itemRepository.save(new Item("testA", 10000, 10));
+        itemRepository.save(new Item("testB", 20000, 20));
+    }
+}
